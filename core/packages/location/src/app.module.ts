2611f22134eec3fdc9c70ff079b228448cc2cb04
dongpp.hcm <dongpp.hcm@kidsplaza.vn>
2025-01-02 08:05:22 +0700

    init source

--- core/packages/location/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LocationModule } from './location/location.module'; // Location module
-
-@Module({
-  imports: [
-    LocationModule, // Add LocationModule to AppModule
-  ],
-})
-export class AppModule {}
